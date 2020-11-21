6efa3697ad9722f6cb34e3f7cfa8ec899b111109
David Gustavsson <david.e.gustavsson@gmail.com>
2020-11-21 14:26:10 +0100

    Rename package from Datax to LaTeXDatax

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Datax
+# LaTeXDatax
 Save specified variables to a data file to be read into a LaTeX document using the accompanying package `datax.sty`.
 
 ## Installation
-`using Pkg;Pkg.add("https://github.com/Datax-package/Datax.jl")`, and install [the datax package [ctan]](https://ctan.org/tex-archive/macros/latex/contrib/datax).
+`using Pkg;Pkg.add("https://github.com/Datax-package/LaTeXDatax.jl")`, and install [the datax package [ctan]](https://ctan.org/tex-archive/macros/latex/contrib/datax).
 
 ## Usage
 ```julia
-using Datax
+using LaTeXDatax
 a = 25;
 b = "Something";
 c = (300,"\\mega\\meter\\per\\second");
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,35 +1,35 @@
-using Unitful,Datax
+using Unitful,LaTeXDatax
 using Test
 
-@testset "Datax.jl" begin
+@testset "LaTeXDatax.jl" begin
     io = IOBuffer();
 
     # Basic printing
-    Datax.printdata(io,"String","%.4g")
+    LaTeXDatax.printdata(io,"String","%.4g")
     @test String(take!(io)) == "String"
-    Datax.printdata(io,4.1,"%.4g")
+    LaTeXDatax.printdata(io,4.1,"%.4g")
     @test String(take!(io)) == "\\num{4.1}"
-    Datax.printdata(io,[1.0,2.4,3.0],"%.4g")
+    LaTeXDatax.printdata(io,[1.0,2.4,3.0],"%.4g")
     @test String(take!(io)) == "\\numlist{1;2.4;3;}"
 
     # Print units
-    Datax.printdata(io,(1.0,"\\meter"),"%.4g")
+    LaTeXDatax.printdata(io,(1.0,"\\meter"),"%.4g")
     @test String(take!(io)) == "\\SI{1}{\\meter}"
-    Datax.printdata(io,([1,2,3],"\\kilogram"),"%.4g")
+    LaTeXDatax.printdata(io,([1,2,3],"\\kilogram"),"%.4g")
     @test String(take!(io)) == "\\SIlist{1;2;3;}{\\kilogram}"
 
     # Format strings
-    Datax.printdata(io,pi,"%.3g")
+    LaTeXDatax.printdata(io,pi,"%.3g")
     @test String(take!(io)) == "\\num{3.14}"
-    Datax.printdata(io,("%.2g",pi),"%.4g")
+    LaTeXDatax.printdata(io,("%.2g",pi),"%.4g")
     @test String(take!(io)) == "\\num{3.1}"
-    Datax.printdata(io,("%.5g",pi,"\\liter"),"%.4g")
+    LaTeXDatax.printdata(io,("%.5g",pi,"\\liter"),"%.4g")
     @test String(take!(io)) == "\\SI{3.1416}{\\liter}"
 
     # Unitful
-    Datax.printdata(io,2u"m","%.4g")
+    LaTeXDatax.printdata(io,2u"m","%.4g")
     @test String(take!(io)) == "\\SI{2}{\\meter}"
-    Datax.printdata(io,[1.4,2.8]u"g/J","%.4g")
+    LaTeXDatax.printdata(io,[1.4,2.8]u"g/J","%.4g")
     @test String(take!(io)) == "\\SIlist{1.4;2.8;}{\\gram\\per\\joule}"
 
 end
